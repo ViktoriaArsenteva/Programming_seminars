2f7a5e398a32d9b61c942ee2f59b8c929938eef6
ViktoriaArsenteva <vika.arsentievaa@yandex.ru>
2022-06-18 01:29:46 +1100

    Решена вторая задача

--- Lesson 1/Task_2_MinMax/Program.cs
+++ Lesson 1/Task_2_MinMax/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число: ");
+string FirstNum = Console.ReadLine();
+int Num1 = int.Parse(FirstNum);
+Console.Write("Введите второе число: ");
+string SecondNum = Console.ReadLine();
+int Num2 = int.Parse(SecondNum);
+if (Num1 > Num2)
+{
+    Console.Write("Большее число: ");
+    Console.WriteLine(Num1);
+    Console.Write("Меньшее число: ");
+    Console.WriteLine(Num2);
+}
+
+else
+{
+    Console.Write("Большее число: ");
+    Console.WriteLine(Num2);
+    Console.Write("Меньшее число: ");
+    Console.WriteLine(Num1);
+}
+
+    
---
+++ Lesson 1/Task_4_Max/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите первое число: ");
+int F = Console.Read();
+Console.WriteLine(F);
