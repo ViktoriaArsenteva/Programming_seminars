76539eeb1175c3b31ab85667d7f720c9b9968bbd
ViktoriaArsenteva <vika.arsentievaa@yandex.ru>
2022-06-19 00:24:34 +1100

    Решена задача 7 - Нахождение последней цифры числа

---
+++ Lesson 1/Task_7_LastNumber/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+Console.Write("Введите число: ");
+String Number = Console.ReadLine();
+double Num = double.Parse(Number);
+Console.WriteLine(Num % 10);
