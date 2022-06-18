6825659463d79fad5c212b2f283b5ed8bc67f877
ViktoriaArsenteva <vika.arsentievaa@yandex.ru>
2022-06-18 23:33:22 +1100

    Решена задача 6 - определение четности числа

---
+++ Lesson 1/Task_6_evenNum/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.Write("Введите число: ");
+string Num = Console.ReadLine();
+double Num1 = double.Parse(Num);
+if (Num1 % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
