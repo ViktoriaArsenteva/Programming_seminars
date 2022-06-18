8532a22217c493cdec06013073ee682976f024a3
ViktoriaArsenteva <vika.arsentievaa@yandex.ru>
2022-06-19 00:16:56 +1100

    Решена задача 5 - вывод промежутка чисел

---
+++ Lesson 1/Task_5_interval/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.Write("Введите число: ");
+string N = Console.ReadLine();
+int N1 = int.Parse(N);
+int N2 = N1 * (-1);
+ for (int i = N2; i <= N1; i++ ) 
+ {
+     Console.Write(i);
+     Console.Write(" ");
+ }
