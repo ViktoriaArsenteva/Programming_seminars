71817da692e42a9c39fbe7545a3af2ea72048d4e
ViktoriaArsenteva <vika.arsentievaa@yandex.ru>
2022-06-18 23:45:10 +1100

    Решена задача 8 - вывод четных чисел от 1 до N

---
+++ Lesson 1/Task_8_Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите число: ");
+string N = Console.ReadLine();
+int N1 = int.Parse(N);
+int i = 2;
+while (i <= N1)
+{
+    Console.Write(i);
+    Console.Write(" ");
+    
+    i+=2;
+}
