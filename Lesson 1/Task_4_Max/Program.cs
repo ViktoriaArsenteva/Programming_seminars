627687e288ce21bbc872312057c3204aadc3d609
ViktoriaArsenteva <vika.arsentievaa@yandex.ru>
2022-06-18 23:15:32 +1100

    Решена задача 4 - поиск максимального значения

--- Lesson 1/Task_4_Max/Program.cs
+++ Lesson 1/Task_4_Max/Program.cs
@@ -1,6 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-Console.WriteLine("Введите первое число: ");
-int F = Console.Read();
-Console.WriteLine(F);
+
+
+
+Console.WriteLine("Введите три числа: ");
+int[] Numbers = new int[] {0,0,0};
+int i = 0;
+int k = 1;
+while (i < Numbers.Length)
+{
+    string Num = Console.ReadLine();
+    Numbers[i] = int.Parse(Num);
+    i++;
+}
+int max = Numbers[0];
+while (k < Numbers.Length)
+{
+    if (Numbers[k]> max)
+    {
+        max = Numbers[k];
+    }
+    k++;
+}
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
