28f20c6147766d94a6ea0590d13ae514dff6dd8b
ViktoriaArsenteva <vika.arsentievaa@yandex.ru>
2022-06-25 00:05:47 +1100

    Решена задача 10 - поиск второй цифры числа

--- Lesson 2/Task_10_SecondNum/Program.cs
+++ Lesson 2/Task_10_SecondNum/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите любое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+List<int> numbers = new List<int>();
+Console.Write($"Вторая цифра числа {num}: ");
+while (num != 0)
+{
+    numbers.Add(num % 10);
+    num /= 10;
+}
+numbers.Reverse();
+Console.WriteLine(numbers[1]);
