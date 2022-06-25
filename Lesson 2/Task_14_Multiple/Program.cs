83c693188d31d71c5961303b45e9dd5902dcfcb6
ViktoriaArsenteva <vika.arsentievaa@yandex.ru>
2022-06-25 22:45:18 +1100

    Добавлена функция в задачу 14

--- Lesson 2/Task_14_Multiple/Program.cs
+++ Lesson 2/Task_14_Multiple/Program.cs
@@ -3,7 +3,12 @@
 int num1 = 7; int num2 = 23;
 Console.Write($"Введите число, чтобы проверить его кратность числам {num1} и {num2}: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number % num1 == 0 && number % num2 == 0)
+int Multiple(int number1, int number2)
+{
+    if (number1 % number2 == 0) return 1;
+    else return 0;
+}
+if (Multiple(number,num1) == 1 && Multiple(number,num2) == 1)
 {
     Console.WriteLine($"Заданное число {number} одновременно кратно числам {num1} и {num2}");
 }
@@ -11,3 +16,4 @@ else
 {
     Console.WriteLine($"Заданоое число {number} не кратно числам {num1} и {num2}");
 }
+
