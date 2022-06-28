af85f2d29379f026a2195cafe69bfcaac40957ac
ViktoriaArsenteva <vika.arsentievaa@yandex.ru>
2022-06-29 00:21:32 +1100

    Решена задача 19 - определение палиндрома

---
+++ Lesson 3/Task_19_Palindrome/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Write("Введите число, чтобы выяснить является ли оно палендромом: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int number = num1;
+List<int> Numbers = new List<int>();
+while (number != 0)
+{
+    Numbers.Add(number % 10);
+    number /= 10;
+}
+int i = 0;
+int lenght = Numbers.Count / 2;
+bool check = false;
+while (lenght != 0)
+{
+    if (Numbers[i] == Numbers[Numbers.Count - 1 - i])
+    {
+        check = true;
+    }
+    else
+    {
+        check = false;
+        break;
+
+    }
+    i++;
+    lenght--;
+}
+if (check == true)
+{
+    Console.WriteLine($"Да! Число {num1} является палиндромом");
+}
+else
+{
+    Console.WriteLine($"Нет! Число {num1} не является палиндромом");
+}
