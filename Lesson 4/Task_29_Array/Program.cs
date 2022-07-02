c6d70e2e0df2ea5c5f179c868e47424cdeabec78
ViktoriaArsenteva <vika.arsentievaa@yandex.ru>
2022-07-02 21:52:37 +1100

    Решена задача 29 - массив

---
+++ Lesson 4/Task_29_Array/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// Задача была решена через генератор псевдослучайных чисел, т.к в задании не указано должен ли их вводить пользователь
+
+int[] Array = new int[8];
+int index = 0;
+var rnd = new Random();
+while (index < Array.Length)
+{
+    Array[index] = rnd.Next(0, 1000);
+    index++;
+}
+for (int i = 0; i < Array.Length; i++)
+{
+    if (i == 0) Console.Write("[");
+    if (i < Array.Length - 1) Console.Write(Array[i] + ",");
+    else Console.Write(Array[i] + "]");
+}
