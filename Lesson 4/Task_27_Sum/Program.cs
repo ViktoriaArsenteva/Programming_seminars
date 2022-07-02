5a9c7d28114ac9c3985bbb1f57c3cdc1073fc420
ViktoriaArsenteva <vika.arsentievaa@yandex.ru>
2022-07-02 21:19:41 +1100

    Решена задаа 27 - сумма цифр числа

---
+++ Lesson 4/Task_27_Sum/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число, чтобы узнать сумму его цифр: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int i = number; i != 0; i /= 10)
+{
+    sum += i % 10;
+}
+Console.WriteLine($"Сумма цифр числа {number} = {sum}");
