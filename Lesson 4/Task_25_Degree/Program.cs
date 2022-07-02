489d5d4db8b4d8983182c1b15ba7bda8c4eadc79
ViktoriaArsenteva <vika.arsentievaa@yandex.ru>
2022-07-02 21:11:44 +1100

    Решена задача 25 - степень числа

---
+++ Lesson 4/Task_25_Degree/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень, в которую необходимо возвести первое число: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+int DegreeOfNumber(int num1, int deg)
+{
+    int result = 1;
+    for (int i = 1; i <= deg; i++)
+    {
+        result *= num1;
+    }
+    return result;
+}
+int answer = DegreeOfNumber(number, degree);
+Console.WriteLine($"Число {number} в степени {degree} = {answer}");
