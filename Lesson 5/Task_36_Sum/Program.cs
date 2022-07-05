e6e78625aaead6a3c6b3611f4763218262bc51a4
ViktoriaArsenteva <vika.arsentievaa@yandex.ru>
2022-07-06 02:10:39 +1100

    Решена задача 36 - сумма чисел неетных индексов

---
+++ Lesson 5/Task_36_Sum/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] CreateArray(int size)
+{
+    int i = 0;
+    int[] Array = new int[size];
+    var rnd = new Random();
+    while (i < Array.Length)
+    {
+        Array[i] = rnd.Next();
+        i++;
+    }
+    return Array;
+}
+
+int SumOfOddNumbers(int[] Array)
+{
+    int index = 0;
+    int sum = 0;
+    while (index < Array.Length)
+    {
+        if (index % 2 != 0) sum += Array[index];
+        index++;
+    }
+    return sum;
+}
+
+void PrintArray(int[] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < Array.Length - 1) Console.Write(Array[i] + ",");
+        else Console.Write(Array[i] + "]");
+    }
+}
+Console.Write("Введите размер массива: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+int[] ArrayRnd = CreateArray(arraySize);
+int result = SumOfOddNumbers(ArrayRnd);
+PrintArray(ArrayRnd);
+Console.WriteLine();
+Console.WriteLine($"Сумма чисел, стоящих на нечетных позициях = {result}");
