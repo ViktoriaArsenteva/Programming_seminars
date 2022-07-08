ecdd39213d1127db323bd64d3b0faf13048572b5
ViktoriaArsenteva <vika.arsentievaa@yandex.ru>
2022-07-09 02:34:01 +1100

    Решена задача 43 - пересечение прямых

--- Lesson 6/Task_43/Program.cs
+++ Lesson 6/Task_43/Program.cs
@@ -1,1 +1,14 @@
 ﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Для нахождения точки пересечения двух прямых, заданных уравнениями y = k1 * x + b1; y = k2 * x + b2 введите значения переменных k1, b1, k2, b2: ");
+Console.Write("k1 = "); double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("b1 = "); double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("k2 = "); double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("b2 = "); double b2 = Convert.ToDouble(Console.ReadLine());
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+double y1 = k2 * x + b2;
+Console.WriteLine(x);
+Console.WriteLine(y);
+;
+Console.WriteLine($"Прямые пересекаются в точке ({x};{y})");
