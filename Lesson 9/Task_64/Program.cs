90888b0fb3544e78cafc00046a51962550ed6c83
ViktoriaArsenteva <vika.arsentievaa@yandex.ru>
2022-07-20 19:30:23 +1100

    Изменено решение задачи 64

--- Lesson 9/Task_64/Program.cs
+++ Lesson 9/Task_64/Program.cs
@@ -1,22 +1,17 @@
 ﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
 void PrintNumbers(int firstNum, int secondNum)
 {
-    for (int i = firstNum; i <= secondNum; i++)
+    if (firstNum == secondNum) Console.Write(secondNum);
+    else 
     {
-        if (i == secondNum)
-        {
-            Console.Write(i);
-        }
-        else
-        {
-            Console.Write($"{i}, ");
-        }
+        Console.Write($"{firstNum}, ");
+        PrintNumbers(firstNum + 1, secondNum);
     }
 }
-Console.WriteLine("Введите два числа, чтобы уидеть все натуральные числа между ними: ");
+Console.WriteLine("Введите два числа, чтобы увидеть все натуральные числа между ними: ");
 Console.Write("Введите первое число: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"Натуральные числа в промежутке [{m},{n}]:");
-PrintNumbers(m,n);
+PrintNumbers(m, n);
